08e9e27cd54cba906f0931d784dddbdfd2c4b0b6
Amirjon Nazarov <a3465172@gmail.com>
2025-01-05 09:58:20 +0500

    f

--- studybud/urls.py
+++ studybud/urls.py
@@ -2,15 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('hello world')
 
-def room(request):
-    return HttpResponse('room')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('room/', room)
 ]
 
