9775412f1e5fbeac1aa959d36d94af00fc658d22
Amirjon Nazarov <a3465172@gmail.com>
2025-01-05 13:51:02 +0500

    f

--- base/views.py
+++ base/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'amir'},
+    {'id': 2, 'name': 'abdolboriy'},
+    {'id': 3, 'name': 'nazar'},
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, 'room.html')
