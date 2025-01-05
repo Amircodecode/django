c60b64bc6efccd3d2ecf21f679dbfcebc0316eaa
Amirjon Nazarov <a3465172@gmail.com>
2025-01-05 19:11:27 +0500

    d

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
 rooms = [
@@ -10,6 +10,7 @@ rooms = [
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
